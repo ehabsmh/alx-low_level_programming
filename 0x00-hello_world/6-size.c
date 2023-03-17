7645e1d63a2cd7d35e42129c30fee9d8e86382e7
ehabsmh <ehabsmh3@gmail.com>
2023-03-16 18:53:45 -0700

    linting fixed in program 02

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char charSize;
-        int intSize;
-        long int longIntSize;
-        long long int longLongIntSize;
-        float floatSize;
-        printf("Size of a char: %lu byte(s)\n", sizeof(charSize));
-        printf("Size of an int: %lu byte(s)\n", sizeof(intSize));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(longIntSize));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntSize));
-        printf("Size of a float: %lu byte(s)\n", sizeof(floatSize));
-        return (0);
+char charSize;
+int intSize;
+long int longIntSize;
+long long int longLongIntSize;
+float floatSize;
+printf("Size of a char: %lu byte(s)\n", sizeof(charSize));
+printf("Size of an int: %lu byte(s)\n", sizeof(intSize));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longIntSize));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntSize));
+printf("Size of a float: %lu byte(s)\n", sizeof(floatSize));
+return (0);
 }
 
