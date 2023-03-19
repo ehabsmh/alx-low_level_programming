0aa3843e930362e83d5f725b8f44f9ab36df4a90
ehabsmh <ehabsmh3@gmail.com>
2023-03-18 18:35:42 -0700

    Program 03 fixed some error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, msg, sizeof(msg));
+	write(STDERR_FILENO, msg, sizeof(msg) - 1);
 	return (1);
 }
