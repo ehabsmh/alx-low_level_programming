79aa61c2268b041b67c6f678b2a91ab65dc6531e
ehabsmh <ehabsmh3@gmail.com>
2023-03-22 09:02:30 -0700

    Program 07, refactored

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
  */
 int print_last_digit(int n)
 {
-n = n % 10;
+	int last_digit = n % 10;
 
-if (n < 0)
-n = -1 * n;
+	if (last_digit < 0)
+	last_digit = -last_digit;
 
-_putchar('0' + n);
-return (n);
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
