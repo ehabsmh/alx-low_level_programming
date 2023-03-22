9b4ef99988331378f31f6c4906ae68f41e3ce7ea
ehabsmh <ehabsmh3@gmail.com>
2023-03-21 19:08:53 -0700

    Error from the checker

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: is the input to check on
+ * @c: is the number to be checked
  *
- * Return: 1 if c is a letter and 0 otherwise
+ * Return: 1 if `c` is a character, and 0 for otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z'))
-	return (1);
-
-	return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _islower(int c);
 int _isalpha(int c);
 
 #endif
+
