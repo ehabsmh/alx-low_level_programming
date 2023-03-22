03706afa4a04fc6e23b13fe9f206cf20ab70b86d
ehabsmh <ehabsmh3@gmail.com>
2023-03-21 19:03:27 -0700

    Program 04 has been modified

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,12 @@
  * _isalpha - checks for alphabetic character
  * @c: is the input to check on
  *
- * Return: 1 if c is a letter and Return: 0 otherwise
+ * Return: 1 if c is a letter and 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 65) && (c <= 90) || (c >= 97) && (c <= 122))
-	{
+	if ((c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z'))
 	return (1);
-	}
 
 	return (0);
 }
