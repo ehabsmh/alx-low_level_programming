c5be66b0a6a2a5ec4a320926bd6a3a8f3c5b0e2d
ehabsmh <ehabsmh3@gmail.com>
2023-04-10 15:48:12 -0700

    static library created

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: string
+ * @accept: substring
+ *
+ * Return: int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ * @s: string
+ * @accept: substring
+ *
+ * Return: s that matches the bytes in accept, otherwise NULL.
+ */
+
+/**
+ * _strpbrk = search a string
+ * */
+char *_strpbrk(char *s, char *accept)
+{
+
+}
