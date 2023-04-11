cdacf6bd6532e69bc8f27f50c9b355163a1f52e2
ehabsmh <ehabsmh3@gmail.com>
2023-04-10 17:58:39 -0700

    program 00 linted

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints the program name
+ * @argc: count of argv
+ * @argv: array of string
+ *
+ * Return: (0)
+ */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - argc]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
