d5b72bf25be6b558e04592da0ec08735c488519c
ehabsmh <ehabsmh3@gmail.com>
2023-04-11 16:10:45 -0700

    Fixed some code - Prog 00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,9 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars
+ * initializes it with a specific char.
+ *
  * @size: Size of bytes
  * @c: Initiaialized character
  *
@@ -17,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
-	if (str = NULL || size == 0)
+	if (str == NULL || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
