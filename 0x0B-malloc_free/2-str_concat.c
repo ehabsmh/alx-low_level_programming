34af7c23353595a4d5fd6c505eab2971c73d9e53
ehabsmh <ehabsmh3@gmail.com>
2023-04-11 19:04:50 -0700

    Issue fixed - Prog 02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,59 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
-/**
- * _strLen - calculate length of string
- * @str: Pointer to a string
- *
- * Return: The length
- */
-int _strLen(char *str)
-{
-	unsigned int i, len = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
- * _concat - concatenate s1 and s2
- * @s1: Destination
- * @s2: Source
- *
- * Return: Destination
- */
-char *_concat(char *s1, char *s2)
-{
-	unsigned int i, s1_len = 0, s2_len = 0;
-	char *result;
-
-	s1_len = _strLen(s1);
-	s2_len = _strLen(s2);
-
-	result = malloc(sizeof(char) * (s1_len + s2_len + 1));
-
-	if (result == NULL)
-		return (NULL);
-
-	for (i = 0; i < s1_len; i++)
-	{
-		result[i] = s1[i];
-	}
-
-	for (i = 0; i < s2_len; i++)
-	{
-		result[s1_len + i] = s2[i];
-	}
-
-	result[s1_len + s2_len] = '\0';
-
-	return (result);
-}
+#include <string.h>
 
 /**
  * str_concat - concatenate two strings pointed to newely string
@@ -65,10 +13,8 @@ char *_concat(char *s1, char *s2)
 char *str_concat(char *s1, char *s2)
 {
 	char *concated_str;
-	unsigned int i, s1_len = 0;
-
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	size_t len1 = 0;
+	size_t len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -76,20 +22,17 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	s1 = _concat(s1, s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-	s1_len = _strLen(s1);
-
-	concated_str = malloc(sizeof(char) * (s1_len + 1));
+	concated_str = malloc(len1 + len2 + 1);
 
 	if (concated_str == NULL)
 		return (NULL);
 
 
-	for (i = 0; i <= s1_len; i++)
-	{
-		concated_str[i] = s1[i];
-	}
+	memcpy(concated_str, s1, len1);
+	memcpy(concated_str + len1, s2, len2 + 1);
 
 	return (concated_str);
 }
