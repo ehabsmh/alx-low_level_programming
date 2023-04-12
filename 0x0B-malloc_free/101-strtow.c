d650345dea17a09f3fd0638bd82799b65cdf3f96
ehabsmh <ehabsmh3@gmail.com>
2023-04-11 19:53:30 -0700

    Program 06 - created only

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - splits a string into words
+ * @str: pointer to string
+ *
+ * Return: double pointer to character
+ */
+
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+	return (str);
+}
