f2c359466c73f4e5a890a09a6713e50c4a442f5e
ehabsmh <ehabsmh3@gmail.com>
2023-04-17 15:45:08 -0700

    dog.h created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: Pointer to dog name
+ * @age: dog age
+ * @owner: Pointer to owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
