ebc9a0cdce7f2a8a1126d5ed6268c31248ec30c6
ehabsmh <ehabsmh3@gmail.com>
2023-04-17 17:55:43 -0700

    ..

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,17 +17,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *name_cpy, *owner_cpy;
 	size_t name_len = strlen(name);
-       	size_t owner_len = strlen(owner);
+	size_t owner_len = strlen(owner);
 
 	/* Allocate memory for the dog struct */
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
 		return (NULL);
 
 	/* Allocate memory for the name string */
 	name_cpy = malloc(sizeof(char) * (name_len + 1));
-
 	if (name_cpy == NULL)
 	{
 		free(new_dog);
@@ -40,7 +38,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/* Allocate memory for the owner string */
 	owner_cpy = malloc(sizeof(char) * (owner_len + 1));
-	
 	if (owner_cpy == NULL)
 	{
 		free(new_dog);
@@ -56,6 +53,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = name_cpy;
 	new_dog->age = age;
 	new_dog->owner = owner_cpy;
-
 	return (new_dog);
 }
