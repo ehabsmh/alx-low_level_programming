1e88f5005fbce503b2e71e68957550358e6015b0
ehabsmh <ehabsmh3@gmail.com>
2023-04-19 16:41:29 -0700

    Program 03 - created and tested

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: Function pointer
+ *
+ * Return: 0 if array or cmp is NULL
+ *	and the index of the element if elem > 0
+ *	OTHERWISE -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+			return (i);
+	}
+
+	return (-1);
+}
