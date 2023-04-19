718e0af2878d9f75e36c19f8af6af85bcf36ef65
ehabsmh <ehabsmh3@gmail.com>
2023-04-19 16:50:08 -0700

    .

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,9 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
