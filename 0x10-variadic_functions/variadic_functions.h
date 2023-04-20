92477c74cdc03719b985c793cc6ab77ea3d6acd8
ehabsmh <ehabsmh3@gmail.com>
2023-04-20 11:54:55 -0700

    ..

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,17 @@
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
+
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 #endif
