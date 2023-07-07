6c09cd5b03ce6c2b82811eba3b675b840a1f01dc
ehabsmh <ehabsmh3@gmail.com>
2023-07-08 00:27:54 +0300

    Indentation fixing

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,28 +1,35 @@
 #include "main.h"
-
+/**
+ *	_strstr - locates a string
+ *	@haystack: string to locate needle
+ *	@needle: string found
+ *
+ * Return pointer OTHERWISE NULL
+ */
 char *_strstr(char *haystack, char *needle)
 {
-  int i, j;
+	int i, j;
 
-  if (*needle == '\0')
-  {
-    return (haystack);
-  }
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 
-  for (i = 0; haystack[i] != '\0'; i++)
-  {
-    j = 0;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
 
-    while (needle[j] != '\0' && haystack[i + j] != '\0' && needle[j] == haystack[i + j])
-    {
-      j++;
-    }
+		while (needle[j] != '\0' && haystack[i + j] != '\0' &&
+					 needle[j] == haystack[i + j])
+		{
+			j++;
+		}
 
-    if (needle[j] == '\0')
-    {
-      return (&haystack[i]);
-    }
-  }
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
 
-  return (NULL);
+	return (NULL);
 }
