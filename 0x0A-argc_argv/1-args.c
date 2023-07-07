450adc4114a4a3eb07f962af5e22196d927c6c78
ehabsmh <ehabsmh3@gmail.com>
2023-07-08 01:03:50 +0300

    1-args.c - solved

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,14 +7,11 @@
  *
  * Return: (0)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
+	if (argc == 1)
+		printf("%d\n", argc - 1);
 
-	argv -= '0';
-	for (i = 0; i < argc; i++)
-	{
-	}
-	printf("%d\n", i - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
