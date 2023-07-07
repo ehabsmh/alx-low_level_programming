7e144177595851ea8aa9de27f142ba05363ec3fc
ehabsmh <ehabsmh3@gmail.com>
2023-07-07 22:12:51 +0300

    Added two tasks

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+  int i, j;
+
+  if (*needle == '\0')
+  {
+    return haystack;
+  }
+
+  for (i = 0; haystack[i] != '\0'; i++)
+  {
+    j = 0;
+
+    while (needle[j] != '\0' && haystack[i + j] != '\0' && needle[j] == haystack[i + j])
+    {
+      j++;
+    }
+
+    printf("\n");
+    if (needle[j] == '\0')
+    {
+      return &haystack[i];
+    }
+  }
+
+  return NULL;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,12 @@
-char *_memset(char *s, char b, unsigned int n);
-char *_memset(char *s, char b, unsigned int n);
+#ifndef MAIN_H
+#define MAIN_H
+#include "stdio.h"
+
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
