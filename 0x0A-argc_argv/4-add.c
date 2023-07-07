bba04718aab92fca74f92ad2e200090ff6b91abd
ehabsmh <ehabsmh3@gmail.com>
2023-07-08 01:20:23 +0300

    Program 4-add.c - solved

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,40 +9,32 @@
  *
  * Return: 0 if program correct 1 if not
  */
+
 int main(int argc, char *argv[])
 {
-	int sum = 0, num = 0;
-	int i;
+	int i, sum = 0;
+	char *ptr;
 
 	if (argc < 2)
 	{
-		printf("0\n");
+		printf("%d\n", 0);
 		return (0);
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-		char *str = argv[i];
-
-		while (*str != '\0')
+		ptr = argv[i];
+		while (*ptr != '\0')
 		{
-		if (!isdigit(*str))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		str++;
-		}
-
-		num = atoi(argv[i]);
-
-		if (num < 0)
-		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(*ptr))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			ptr++;
 		}
 
-		sum += num;
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
