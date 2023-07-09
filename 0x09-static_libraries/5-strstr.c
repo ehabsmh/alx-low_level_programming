637fc4aac67aff54fd1908c1354ba12a0ec9c567
ehabsmh <ehabsmh3@gmail.com>
2023-07-09 16:38:16 +0300

    DOS files converted to UNIX

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,34 +1,30 @@
 #include "main.h"
 /**
- *	_strstr - locates a string
- *	@haystack: string to locate needle
- *	@needle: string found
+ * _strstr - locates a string
+ * @haystack: string to locate needle
+ * @needle: string found
  *
- * Return pointer OTHERWISE NULL
+ * Return: pointer, OTHERWISE NULL
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
 	if (*needle == '\0')
-	{
 		return (haystack);
-	}
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		j = 0;
 
-		while (needle[j] != '\0' && haystack[i + j] != '\0' &&
-					 needle[j] == haystack[i + j])
+		while ((needle[j] != '\0') && (haystack[i + j] != '\0') &&
+					 (needle[j] == haystack[i + j]))
 		{
 			j++;
 		}
-
 		if (needle[j] == '\0')
-		{
 			return (&haystack[i]);
-		}
 	}
 
 	return (NULL);
