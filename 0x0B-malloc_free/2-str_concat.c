038beb1fd9fa2d63c31b29b9b31623638395fdc9
ehabsmh <ehabsmh3@gmail.com>
2023-07-10 22:26:42 +0300

    Fixed NULL passed to arguments, to be treated as an empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -53,14 +53,14 @@ char *str_concat(char *s1, char *s2)
 	int s1_len, s2_len;
 	char *str;
 
-	s1_len = _strlen(s1);
-	s2_len = _strlen(s2);
-
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
 
 	str = malloc((sizeof(char) * (s1_len + s2_len + 1)));
 
