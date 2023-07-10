a2e403437e280d553c4817d95cfaf3e41886236b
ehabsmh <ehabsmh3@gmail.com>
2023-07-10 22:01:54 +0300

    2-str_concat.c - released

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,20 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
 
 /**
- * str_concat - concatenate two strings pointed to newely string
- * @s1: Dest
- * @s2: Src
+ * _strlen - Evaluates the length of a string
+ * @s: array of strings (decayed into a pointer when passed as an argument)
  *
- * Return: newely pointer
+ * Return: the length of the string
  */
+
+int _strlen(char *s) /* char *s = str; */
+{
+	unsigned int str_len = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		str_len++;
+	}
+
+	return (str_len);
+}
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination where copied at
+ * @src: the copy will take from
+ * @n: number of bytes
+ *
+ * Return: pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
+/**
+ * str_concat - allocates newely space for two concatenated strings
+ * @s1: The destination string to be concatenated by s2
+ * @s2: The source string that concatenated to s2
+ *
+ * Return: NULL if malloc failed OTHERWISE,
+ * Return pointer
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *concated_str;
-	size_t len1 = 0;
-	size_t len2 = 0;
+	int s1_len, s2_len;
+	char *str;
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,17 +62,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	str = malloc((sizeof(char) * s1_len + 1 + s2_len + 1));
 
-	concated_str = malloc(len1 + len2 + 1);
-
-	if (concated_str == NULL)
+	if (str == NULL)
 		return (NULL);
 
+	_memcpy(str, s1, s1_len + 1);
+	_memcpy(str + s1_len, s2, s2_len + 1);
 
-	memcpy(concated_str, s1, len1);
-	memcpy(concated_str + len1, s2, len2 + 1);
-
-	return (concated_str);
+	return (str);
 }
