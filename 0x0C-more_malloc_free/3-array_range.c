3574c0f1d4c1631c77cf8301c3d821b8ea36d8c8
ehabsmh <ehabsmh3@gmail.com>
2023-07-14 02:34:55 +0300

    3-array_range.c - released

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,20 +12,22 @@
 
 int *array_range(int min, int max)
 {
-	int *allocated_array, size, i;
+	int i, *ptr, size;
 
 	if (min > max)
 		return (NULL);
 
 	size = max - min + 1;
 
-	allocated_array = malloc(size * sizeof(int));
+	ptr = malloc(size * sizeof(int));
 
-	if (allocated_array == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		allocated_array[i] = min + i;
+	{
+		ptr[i] = min + i;
+	}
 
-	return (allocated_array);
+	return (ptr);
 }
