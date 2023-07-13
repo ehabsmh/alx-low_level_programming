5d30942c96680e151d2bf983c83660f2d0ab83cb
ehabsmh <ehabsmh3@gmail.com>
2023-07-13 17:50:54 +0300

    1-string_nconcat.c - released

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,89 @@
 #include "main.h"
 
 /**
- * string_nconcat - Allocates memory for two concated strs
- * @s1: pointer to the dest
- * @s2: pointer to the src
- * @n: number of bytes or characters
+ * _strlen - Evaluates the length of a string
+ * @s: array of strings (decayed into a pointer when passed as an argument)
  *
- * Return: Pointer to the allocated memory
+ * Return: the length of the string
+ */
+
+int _strlen(char *s) /* char *s = str; */
+{
+	unsigned int str_len = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		str_len++;
+	}
+
+	return (str_len);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src to the buffer
+ * pointed to by dest
+ * @dest: Pointer to the buffer
+ * @src: pointer to string
+ *
+ * Return: pointer to the string which copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *str;
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	str = dest;
+
+	return (str);
+}
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: destination string
+ * @s2: source string will be appended to s1
+ * @n: number of the appended bytes in s2.
+ *
+ * Return: NULL if malloc fails
+ * OTHERWISE, return pointer to the newely allocated space
+ * in heap
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s1_len, s2_len, i;
-	char *concated_str;
+	char *str;
 
-	/*Check Null values*/
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	/*Calculating the length*/
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
 
-	/*assigns length of s2 to n*/
 	if (n >= s2_len)
 		n = s2_len;
 
-	concated_str = malloc(s1_len + n + 1);
+	str = malloc((s1_len + n + 1) * sizeof(char));
 
-	if (concated_str == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	/*Copying s1 to concated_str*/
-	for (i = 0; i < s1_len; i++)
-		concated_str[i] = s1[i];
+	_strcpy(str, s1);
 
-	/*Append s2 to concated_str*/
 	for (i = 0; i < n; i++)
-		concated_str[s1_len + i] = s2[i];
+		str[s1_len + i] = s2[i];
 
-	concated_str[s1_len + i] = '\0';
+	str[s1_len + i] = '\0';
 
-	return (concated_str);
+	return (str);
 }
