db320bd5739f74e5b795b05e10c2ce259bb13a39
ehabsmh <ehabsmh3@gmail.com>
2023-07-14 02:28:48 +0300

    2-calloc.c - released

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,30 +1,46 @@
 #include "main.h"
 
 /**
- * _calloc - Allocates memory for an array
- * @nmemb: Number of blocks
- * @size: Size of blocks
+ * _memset - fills the memory with constant byte
+ * @s: pointer to the buffer filled with the constant byte
+ * @b: the constant byte that will be filled with
+ * @n: number of the constant byte
  *
- * Return: Void pointer
+ * Return: pointer to the buffer
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements
+ * @size: size of datatype
+ *
+ * Return: NULL if malloc fails, nmemb is 0 or size is 0
+ * OTHERWISE Return pointer to the allocated memory.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *allocated_array;
-	unsigned int total_size;
-
-	total_size = nmemb * size;
+	void *calloc;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	allocated_array = malloc(total_size);
+	calloc = malloc(nmemb * size);
 
-	if (allocated_array == NULL)
+	if (calloc == NULL)
 		return (NULL);
 
-	/*Set all blocks to 0*/
-	memset(allocated_array, 0, total_size);
+	_memset(calloc, 0, nmemb);
 
-	return (allocated_array);
+	return (calloc);
 }
