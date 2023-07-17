92aa11e34636725ea62082952f76575f4a71ea9b
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-18 01:06:35 +0300

    nthng imp

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,8 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s);
-
-		if (s[i + 1] == c)
-			return (s + 1);
+			return (s + i);
 	}
 
-	return (NULL);
+	return ('\0');
 }
