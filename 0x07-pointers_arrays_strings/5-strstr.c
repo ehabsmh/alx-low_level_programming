8154870473a722846928acb650e3264da06ab4a8
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-18 00:59:45 +0300

    nthng imp

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,37 @@
 #include "main.h"
 
+/**
+ * _strstr - find substring in initial string
+ * @haystack: initial string to be searched
+ * @needle: substring to be found
+ *
+ * Return: substring if found, NULL if not found
+ */
+
 char *_strstr(char *haystack, char *needle)
 {
-  int i, j;
+	int i, j;
 
-  if (*needle == '\0')
-  {
-    return (haystack);
-  }
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 
-  for (i = 0; haystack[i] != '\0'; i++)
-  {
-    j = 0;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
 
-    while (needle[j] != '\0' && haystack[i + j] != '\0' && needle[j] == haystack[i + j])
-    {
-      j++;
-    }
+		while (needle[j] != '\0' && haystack[i + j] != '\0'
+		&& needle[j] == haystack[i + j])
+		{
+			j++;
+		}
 
-    if (needle[j] == '\0')
-    {
-      return (&haystack[i]);
-    }
-  }
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
 
-  return (NULL);
+	return (NULL);
 }
