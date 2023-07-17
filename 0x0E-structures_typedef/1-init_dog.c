64fb2badf1dff0ee028cf1fb8c05f90e5c8a735e
root <root@Ehab.localdomain>
2023-07-17 21:51:48 +0300

    1-init_dog.c - Refactored, Description changed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,19 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
- * @d: pointer struct dog to initialise
- * @name: pointer to dog name
+ * init_dog - initializes a dog of type dog
+ * @d: pointer to variable of struct dog
+ * @name: pointer to the dog name
  * @age: dog age
- * @owner: pointer to owner of dog
- *
- * Return: void
+ * @owner: pointer to the dog owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
+	if (!d)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
 }
