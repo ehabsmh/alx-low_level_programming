8becc2f4914d83ceac6d1f254fd645158ba5fe40
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-20 23:28:39 +0300

    added new line after the function returns

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,18 +10,22 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n > 0)
+	if (n == 0)
 	{
-		va_start(args, n);
-
-		for (i = 1; i <= n; i++)
-		{
-			printf("%d", va_arg(args, int));
-
-			if (i != n && separator)
-				printf("%s", separator);
-		}
+		printf("\n");
+		return;
 	}
+
+	va_start(args, n);
+
+	for (i = 1; i <= n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (i != n && separator)
+			printf("%s", separator);
+	}
+
 	va_end(args);
 	printf("\n");
 }
