d7fed2215aa6c2fa8fab6730a7f13ddbbb11c2c8
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-20 23:24:01 +0300

    nthng imp

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,17 +10,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n > 0)
+	if (n <= 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 1; i <= n; i++)
 	{
-		va_start(args, n);
+		printf("%d", va_arg(args, int));
 
-		for (i = 1; i <= n; i++)
-		{
-			printf("%d", va_arg(args, int));
-
-			if (i != n && separator)
-				printf("%s", separator);
-		}
+		if (i != n && separator)
+			printf("%s", separator);
 	}
 
 	va_end(args);
