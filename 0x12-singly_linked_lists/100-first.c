cf7794c67ca9bd6c3bebbadf2076d337dbdee657
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-28 05:37:13 +0300

    100-first.c - released

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * startup_fn - executes before main
+ *
+ * Return: void
+*/
+void startup_fn(void)
+{
+	puts("You're beat! and yet, you must allow,\nI bore my house upon my back!");
+}
