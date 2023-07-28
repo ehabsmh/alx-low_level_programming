5df5b75ec6ee00244dd3671506bc17b149370bcd
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-28 05:31:45 +0300

    memory freed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the linked list
+ * @head: first node
+ *
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+
+	if (!head)
+		return;
+
+	while (ptr)
+	{
+		free(ptr->next);
+		free(ptr->str);
+		ptr = ptr->next;
+	}
+
+	free(head);
+}
