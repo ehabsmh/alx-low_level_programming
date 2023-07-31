5ff2e627cdcff64a978c41c508e0220242033be8
Ehab Elsayed <ehabsmh3@gmail.com>
2023-07-31 16:57:33 +0300

    Betty linting - added

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,25 +4,25 @@
  * print_listint - Prints all the elements of a list
  * @h: Pointer to the head of list
  *
- * Return : Number of nodes
+ * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-  size_t number_nodes;
-  const listint_t *traversal;
+	size_t number_nodes;
+	const listint_t *traversal;
 
-  if (!h)
-    return (0);
+	if (!h)
+		return (0);
 
-  traversal = h;
-  number_nodes = 0;
+	traversal = h;
+	number_nodes = 0;
 
-  while (traversal)
-  {
-    printf("%d\n", traversal->n);
-    traversal = traversal->next;
-    number_nodes++;
-  }
+	while (traversal)
+	{
+		printf("%d\n", traversal->n);
+		traversal = traversal->next;
+		number_nodes++;
+	}
 
-  return (number_nodes);
+	return (number_nodes);
 }
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -3,7 +3,7 @@
 ### **This repository contains Fifteen programs related to Singly Linked List. The programs were written in C language.**
 
 [Program 00: List length](https://github.com/ehabsmh/alx-low_level_programming/blob/main/0x13-more_singly_linked_lists/1-listint_len.c)
-returns the number of elements in a linked listint_t list. 
+returns the number of elements in a linked listint_t list.
 
 The prototype for the function is:
 ```
