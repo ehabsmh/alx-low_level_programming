6b106cd561918dcc0041302d64d173fe5e92f67c
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 16:06:33 +0300

    listint_len included at the file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,3 +46,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (new_node);
 }
+
+size_t listint_len(const listint_t *h)
+{
+	size_t number_nodes;
+	const listint_t *traversal;
+
+	if (!h)
+		return (0);
+
+	traversal = h;
+	number_nodes = 0;
+
+	while (traversal)
+	{
+		traversal = traversal->next;
+		number_nodes++;
+	}
+
+	return (number_nodes);
+}
