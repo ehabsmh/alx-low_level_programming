a8554e44565c12cb7b7903dd3f28ba7b35ed33f4
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 22:29:43 +0300

    testing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (idx > 1 && traverse)
 	{
-		traverse = (*head)->next;
+		traverse = traverse->next;
 		idx--;
 	}
 
