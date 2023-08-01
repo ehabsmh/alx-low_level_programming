36b9bd6e27cff45e8bf23f7adb2ddeb881e65910
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 16:04:51 +0300

    NULL returned if index is bigger than the length of linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *traverse;
 
 	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (!new_node || idx > listint_len(*head))
 		return (NULL);
 
 	new_node->n = n;
