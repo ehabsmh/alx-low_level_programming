24f7c2284b63f8b0790375aac79f60f3e4a0912b
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 23:22:47 +0300

    Betty style added

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,22 +1,28 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - Reverses a linked list
+ * @head: Pointer to the head pointer
+ *
+ * Return: pointer to the reversed linked list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-  listint_t *prev = NULL;
-  listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-  if (!*head)
-    return (NULL);
+	if (!*head)
+		return (NULL);
 
-  while (*head)
-  {
-    next = (*head)->next;
-    (*head)->next = prev;
-    prev = *head;
-    *head = next;
-  }
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
 
-  *head = prev;
+	*head = prev;
 
-  return (*head);
+	return (*head);
 }
