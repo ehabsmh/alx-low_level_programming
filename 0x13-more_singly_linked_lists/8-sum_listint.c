8582d341e5cb07abe7cbcc3d27861264185f01a8
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 14:31:09 +0300

    betty style added

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,27 @@
 #include "lists.h"
 
+/**
+ * sum_listint - Returns the sum of all data (n) of the LS
+ * @head: Pointer to the first node
+ *
+ * Return: the sum of all data, OTHERWISE return 0
+ */
 int sum_listint(listint_t *head)
 {
-  int sum;
-  listint_t *traverse;
+	int sum;
+	listint_t *traverse;
 
-  if (!head)
-    return (0);
+	if (!head)
+		return (0);
 
-  traverse = head;
-  sum = 0;
+	traverse = head;
+	sum = 0;
 
-  while (traverse)
-  {
-    sum += traverse->n;
-    traverse = traverse->next;
-  }
+	while (traverse)
+	{
+		sum += traverse->n;
+		traverse = traverse->next;
+	}
 
-  return (sum);
+	return (sum);
 }
