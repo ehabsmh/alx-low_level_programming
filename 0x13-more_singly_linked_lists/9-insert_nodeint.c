fbd5978f53357e7fece11eb6b36e23d6e593375e
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 16:44:40 +0300

    code reverted

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,11 +33,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (idx > 1 && traverse)
 	{
-		idx--;
 		traverse = traverse->next;
+		idx--;
 	}
 
-	if (!traverse || !traverse->next)
+	if (!traverse)
 	{
 		free(new_node);
 		return (NULL);
