569d7ce10197030c081765e0eb90a46122f4ba10
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-01 16:31:59 +0300

    program handling

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	traverse = *head;
 
+	if (!traverse)
+		return (NULL);
+
 	while (idx > 1 && traverse)
 	{
 		traverse = traverse->next;
