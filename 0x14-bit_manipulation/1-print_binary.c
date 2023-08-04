5ceae3cd49654ead268945d96d8f6ef22d7e3ab0
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-04 05:05:28 +0300

    1-print_binary.c - released

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n : the number to be printed
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+
+{
+	char i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	i = (n & 1) ? '1' : '0';
+	_putchar(i);
+}
