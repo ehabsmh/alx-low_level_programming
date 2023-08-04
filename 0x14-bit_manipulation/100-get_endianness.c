3a93364b37d037fc7065b1dbad833da0c477c122
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-04 05:31:28 +0300

    100-get_endianness.c - released

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks if the system is big endian or little endian
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+
+{
+	int num;
+	char *adrs;
+
+	num = 2;
+	adrs = (char *)&num;
+	if (*adrs)
+		return (1);
+	return (0);
+}
