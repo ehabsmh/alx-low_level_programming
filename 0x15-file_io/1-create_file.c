2b6e48fd32ed5494e486beb8e7d27f3b625bcc61
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-08 05:51:28 +0300

    text content handled

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
+
 	num_written = write(fd, text_content, _strlen(text_content));
 	if (num_written < 0)
 		return (-1);
