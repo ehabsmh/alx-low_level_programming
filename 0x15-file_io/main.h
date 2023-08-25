2a10f993ed76e37372584b7f3b65aa7ca6f4e947
Ehab Elsayed <ehabsmh3@gmail.com>
2023-08-25 22:39:26 +0300

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <elf.h>
+
+
 
 /* HELPER PROTOTYPES */
 int _putchar(char c);
@@ -14,4 +20,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
