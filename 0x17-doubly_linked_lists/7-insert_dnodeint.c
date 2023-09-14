04358628a83e6046dffe5f7b331eb8dbcdbc2a78
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-15 00:40:09 +0300

    if head is NULL should return NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,11 +15,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *traverse = *h;
 	dlistint_t *nth_node = NULL;
 
-	if (!new)
+	if (!new || !*h)
 		return (NULL);
 
-	/* If linked list is empty or index == 0: add node at beginning */
-	if (!traverse || idx == 0)
+	/* If index == 0: add node at beginning */
+	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
@@ -29,9 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (!nth_node->next)
-	{
 		return (add_dnodeint_end(h, n));
-	}
 
 	new->next = nth_node;
 	new->n = n;
