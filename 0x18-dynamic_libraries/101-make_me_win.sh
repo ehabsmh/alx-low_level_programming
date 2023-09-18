5046396e9ad854e493a9d9ec2f8461ed833ed4ce
Ehab Elsayed <ehabsmh3@gmail.com>
2023-09-18 21:05:24 +0300

    .

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ehabsmh/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp/ https://github.com/ehabsmh/alx-low_level_programming/blob/main/0x18-dynamic_libraries/lib_hack.so
 export LD_PRELOAD=/tmp/lib_hack.so
