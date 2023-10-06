ef630377975290a69494f741a7421b189da343b5
Ehab Elsayed <ehabsmh3@gmail.com>
2023-10-06 21:35:25 +0300

    refactored

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -73,9 +73,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* Getting the key index */
 	k_index = key_index((unsigned char *)key, ht->size);
 
-	if (k_index == 0)
-		return (0);
-
 	arr = ht->array;
 	item = arr[k_index];
 
