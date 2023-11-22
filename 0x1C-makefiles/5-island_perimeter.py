6182e40e9f9ba1948ac39e74c85ecffea9566f1d
Ehab Elsayed <ehabsmh3@gmail.com>
2023-11-22 20:33:59 +0200

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in the given grid.
-    """
-
+    """Search perimeter"""
     perimeter = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
+    for a in range(len(grid)):
+        for b in range(len(grid[a])):
+            if grid[a][b]:
                 perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
+                if a > 0 and grid[a - 1][b] == 1:
                     perimeter -= 2
 
-                if j > 0 and grid[i][j - 1] == 1:
+                if b > 0 and grid[a][b - 1] == 1:
                     perimeter -= 2
 
     return perimeter
