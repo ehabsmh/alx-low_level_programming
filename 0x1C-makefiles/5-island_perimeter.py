6d6d91b4a77c35ae4875246e63daf8789e08aaa7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-11-22 20:32:49 +0200

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,19 @@
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in the given grid.
-
-    Args:
-        grid: Represents the island with 0 for water and 1 for land.
-
-    Returns:
-        int: The perimeter of the island.
     """
 
     perimeter = 0
 
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                # Check adjacent cells and subtract the common edges
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
+
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
