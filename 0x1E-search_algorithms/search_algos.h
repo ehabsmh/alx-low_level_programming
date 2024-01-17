1e42c109d086807485959f0defc7174e0f1aaf83
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-18 01:18:36 +0200

    11. Advanced binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ void print_array(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
