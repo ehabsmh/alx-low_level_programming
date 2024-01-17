ebabeaed32e6cc649afe331eb5c8b20ccfd1f17e
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-18 01:13:52 +0200

    10. Exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,11 +2,14 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
